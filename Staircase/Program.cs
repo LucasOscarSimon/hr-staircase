51f21a6b8a4b00cbfaca2defc0cf471b4ad2da8a
Lucas Simon <lucas.oscar.simon@gmail.com>
2017-08-27 22:14:07 -0300

    Draws a right-aligned staircase

--- Staircase/Program.cs
+++ Staircase/Program.cs
@@ -10,6 +10,22 @@ namespace Staircase
     {
         static void Main(string[] args)
         {
+            StringBuilder builder = new StringBuilder();
+            int totalNumberOfSteps = Convert.ToInt32(Console.ReadLine());
+            var cont = 0;
+            for (int stp = totalNumberOfSteps - 1; stp >= 0; stp--)
+            {
+                builder.Append(' ', stp);
+                cont++;
+                if (builder.Length == stp)
+                    builder.Append('#',cont);
+                else
+                    builder.Append('#', cont);
+                builder.AppendLine();
+            }
+            
+            Console.Write(builder);
+            Console.ReadLine();
         }
     }
 }
